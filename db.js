c87b9b8dc47873a25a1955c40a811e2d92bdac12
manju.gk@accubits.com <manju.gk@accubits.com>
2022-08-04 13:23:35 +0530

    Connecting MYSQL

---
+++ db.js
@@ -0,0 +1,28 @@
+var mysql = require('mysql');
+var db_config={
+  host : '127.0.0.1',
+  user:'root',
+  password:'qwerty123456',
+  database: 'webapp'
+}
+
+var connection;
+
+function handleDisconnect(){
+  connection = mysql.createConnection(db_config);
+  connection.connect(function(err){
+    if(err){
+      console.log('error when connecting to db:',err);
+      setTimeout(handleDisconnect,2000);
+    }
+  });
+  connection.on('error',function(err){
+    if(err.code=== 'PROTOCOL_CONNECTION_LIST'){
+     handleDisconnect();
+   }else{
+     throw err;
+   }
+  });
+}
+handleDisconnect();
+module.exports = connection;
