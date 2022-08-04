8e785ae3aa1b8af89a79db2e859d5e1ad109514f
manju.gk@accubits.com <manju.gk@accubits.com>
2022-08-04 11:20:31 +0530

    Post input setup

--- server.js
+++ server.js
@@ -1,26 +1,17 @@
 const express= require("express");
 const app= express();
-const bodyParser = require('bodyParser');
 
+app.post('/',function(req,res){
+   var email=req.body.email;
+   var amount=req.body.amount;
 
-//app.use(bodyParser.json());
-
-app.post('/',(req,res)=>{
-  var email= req.body.email;
-  var amount=req.body.amount;
-
-
-  //if(amount <=1){
-    //return_info={}
-  //  return_info.error = true;
-    //return_info.message="The amount should be greater than 1";
-    //return res.send(return_info);
-
-  }
-  res.send({"amount":amount,"email":email});
+  res.send({"amount";amount,"email":email });
 });
 
-app.listen(3000,(){
+
+
+
+app.listen(3000,function(){
    console.log('server is running on port 3000');
 
 });
