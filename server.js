c9ea7e2bbbb262088cb908bad882a1318025a66e
manju.gk@accubits.com <manju.gk@accubits.com>
2022-08-05 19:12:27 +0530

    Getting data back

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ app.post('/', async (req,res)=>{
   res.send(result);
 });
 
+app.get("/get_total_amount", async (req,res)=>{
+   var result = await get_total_amount();
+  res.send(result);
+});
+
+
 
 
 
