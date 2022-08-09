a8eb62af54edf4a075f2847ab082c8465ea9324d
manju.gk@accubits.com <manju.gk@accubits.com>
2022-08-09 23:31:49 +0530

    Storing a session

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const {save_user_information} = require('./models/server_db');
 const path=require('path');
 const publicPath = path.join(__dirname,'./public');
 const paypal=require('paypal-rest-sdk');
+const session=require('express-session');
 
+app.use(session(
+  {secret:'my web app',
+  cookie :{maxAge:60000}
+}
+));
 
 app.use(bodyParser.json());
 app.use(express.static(publicPath));
@@ -29,6 +35,9 @@ app.post('/post_info', async (req,res)=>{
    }
   var fee_amount=amount * 0.9;
   var result = await save_user_information({"amount":fee_amount,"email":email});
+
+  req.session.paypal_amount=amount;
+
 
   var create_payment_json = {
    "intent": "sale",
@@ -87,7 +96,7 @@ app.get('/success',(req,res)=>{
      "transactions":[{
        "amount":{
          "currency":"USD",
-         "total":100
+         "total":  req.session.paypal_amount
        }
      }]
    };
