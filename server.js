555a769d2836a4fe0b6d2a95788ed7c925a7b3bd
manju.gk@accubits.com <manju.gk@accubits.com>
2022-08-09 19:36:59 +0530

    installing paypal sdk

--- server.js
+++ server.js
@@ -4,10 +4,18 @@ const bodyParser=require("body-parser");
 const {save_user_information} = require('./models/server_db');
 const path=require('path');
 const publicPath = path.join(__dirname,'./public');
+const paypal=require('paypal-rest-sdk');
+
 
 app.use(bodyParser.json());
 app.use(express.static(publicPath));
 
+/*paypal configuration*/
+paypal.configure({
+  'mode': 'sandbox', //sandbox or live
+  'client_id': 'Afs5p5MEQGIpH1gycXP3AtVfTj5q0lsbp3X8Ckb9lCiWN3c_5xnIVrB8-WiA4rmWm5QyciUmQqwLFkxd',
+  'client_secret': 'EM6dzbIl-5LO5kb6B_Mqg2LgnmyOb6czzboZALNrowT8LVo_1cuuIHZRebiKpTDwcMywypzgwaDe2MCk'
+});
 
 app.post('/post_info', async (req,res)=>{
    var email=req.body.email;
