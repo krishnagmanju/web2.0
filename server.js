c55b29416881fa1f3fd625b4a42d210218807075
manju.gk@accubits.com <manju.gk@accubits.com>
2022-08-09 23:44:56 +0530

    Picking the winner

--- server.js
+++ server.js
@@ -117,6 +117,59 @@ app.get("/get_total_amount", async (req,res)=>{
   res.send(result);
 });
 
+app.get('/pick_winner', async(req,res)=>{
+  var result = await get_total_amount();
+  var total_amount = result[0].total_amount;
+  req.session.paypal_amount =total_amount;
+
+/*create paypal payment*/
+  var create_payment_json = {
+   "intent": "sale",
+   "payer": {
+       "payment_method": "paypal"
+   },
+   "redirect_urls": {
+       "return_url": "http://localhost:3000/success",
+       "cancel_url": "http://localhost:3000/cancel"
+   },
+   "transactions": [{
+       "item_list": {
+           "items": [{
+               "name": "Lottery",
+               "sku": "Funding",
+               "price": amount,
+               "currency": "USD",
+               "quantity": 1
+           }]
+       },
+       "amount": {
+           "currency": "USD",
+           "total": req.session.paypal_amount
+       },
+       "payee":{
+         'email':winner_email
+       },
+       "description": "paying the winner of the lottery application"
+   }]
+  };
+
+  paypal.payment.create(create_payment_json, function (error, payment) {
+     if (error) {
+         throw error;
+     } else {
+         console.log("Create Payment Response");
+         console.log(payment);
+         for (var i=0; i< payment.links.length;i++){
+           if(payment.links[i].rel == 'approval_url'){
+             return res.send(payment.links[i].href);
+           }
+         }
+     }
+  });
+
+
+
+});
 
 
 
